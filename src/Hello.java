4a7a8728bcf736b5200d256e1e7c62373f34d0fb
NOtahina <andriamananjaranotahina@gmail.com>
2023-09-27 14:00:51 +0300

    Hello update

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,8 @@
 package Hello;
 
 import java.io.*;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.util.*;
 
 public class Hello extends HttpServlet {
